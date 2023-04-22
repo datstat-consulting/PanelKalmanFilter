28d5dd75b6d3aaef6fd40d329c8152dde44488ea
datstat-consulting <85887371+datstat-consulting@users.noreply.github.com>
2023-04-23 00:46:27 +0800

    Update PanelKalmanFilter.py

--- PanelKalmanFilter.py
+++ PanelKalmanFilter.py
@@ -15,9 +15,11 @@ class PanelKalmanFilter():
     - `params_init`: our parameter initialization
     One will have to edit the parameters for different configurations.
     """
-    def __init__(self, endog, exog, stateDim, obsDim):
+    def __init__(self, stateDim, obsDim, endog, exog = None):
         self.stateDim = stateDim
         self.obsDim = obsDim
+        self.endog = endog
+        self.exog = exog
         self.post_exp = []
         #self.K = len(data.columns.levels[0]) - 1
         self.K = len(exog)
@@ -29,6 +31,12 @@ class PanelKalmanFilter():
         self.obsDict = []
         for i in range(0,stateDim*obsDim):
             self.obsDict.append(list(endog.columns[0+i*stateDim*obsDim:stateDim*obsDim*(i+1)]))
+        self.defaultInitParams = [1.69271632, -7.53703822, -4.23337738, 7.737883 ,-0.34051513, -0.9349601,
+                                -1.81282109,-2.00712979,-1.87171474,5.94652967,6.35815269,4.86795731,
+                                -1.58287851,-1.23747189,-1.41927504,-2.26545219,-1.5234699,2.08084558,
+                                -3.76331173,4.46234078,-2.33860228,1.35042423,3.35580212,2.1897442,
+                                -1.81117239, -1.83429089, 3.54230905, 4.98408931, -6.51527362, 2.03645846,
+                                2.32582213, 0.85892667, -3.80231989, 4.16718004]
 
     """The following function is a helper function meant to work with other more complex functions. It reshapes a list of initial parameters into their proper forms.
     - `A`: diagonal matrix of dimension `stateDim`$\times$`stateDim`. This matrix holds parameters for the state equation.
@@ -38,54 +46,58 @@ class PanelKalmanFilter():
     - `B`: matrix for exogenous variable parameters. It is $N \times K$, with a row for each group, and a column for each independent variable.
     - `Sigma2MLE`: initialized variance for exogenous parameters.
     """
-    def unpackParams(params):
+    def unpackParams(self, params):
         place = 0
         A = np.reshape(params[place:(place + self.stateDim**2)], (self.stateDim, self.stateDim))
-        place += stateDim**2
+        place += self.stateDim**2
         V = np.diag(np.exp(params[(place):(place+self.stateDim)]))
-        place += stateDim
+        place += self.stateDim
         C = np.zeros(shape = (self.stateDim*self.obsDim,self.stateDim))
         for j in range(0,self.stateDim):
             C[(self.obsDim*(j)):self.obsDim*(j+1),j] = params[place+self.obsDim*(j):(place+(self.obsDim)*(j+1))]
         place += self.obsDim*self.stateDim
         W = np.diag(np.exp(params[(place):(place+self.obsDim*self.stateDim)]))
         place += self.obsDim*self.stateDim
-        B = np.zeros(shape = (self.N,self.K))
-        for j in range(0, self.K):
-            B[:,j] = params[place+(self.N)*(j):place+(self.N)*(j+1)]
-        place += self.N*self.K
-        sigma2MLE = params[place]
+
+        if self.exog is None:
+            B = None
+            sigma2MLE = None
+        else:
+            B = np.zeros(shape = (self.N, self.K))
+            for j in range(0, self.K):
+                B[:,j] = params[place+(self.N)*(j):place+(self.N)*(j+1)]
+            place += self.N*self.K
+            sigma2MLE = params[place]
+
         return [A,V,C,W,B, sigma2MLE]
 
+    """
+    This function estimates the Kalman Filter on time increments, using observations per time. 
+    This code also calculates likelihood for the linear model $y = Az + Bx + w$, 
+    where $w$ is the error term, $x$ is a vector of exogenous variables, and $z$ is the state variable.
+    """
 
-#stateDim = 2
-#obsDim = 3
-
-#params_init = [1.71980540,-7.84056667,-4.17814607,7.18531777,-1.30677476,
-#-1.18088726,-1.66055959,-1.57007816,-1.55171518,5.83622406,6.27800947,4.49955609,
-#-1.51563347,-1.59148527,-1.44753769,-1.84256431,-1.69543580,1.97653226,-3.36927402,5.30981140,
-#-2.57127719,1.79427074,3.76140164,2.04323101,-1.35583690,-1.30289481,3.46299721,5.46711510,
-#-5.99897917,2.01711379,2.65831208,1.38660559,-3.92584823,3.47669802]
-
-    """This function estimates the Kalman Filter on time increments, using observations per time. This code also calculates likelihood for the linear model $y = Az + Bx + w$, where $w$ is the error term, $x$ is a vector of exogenous variables, and $z$ is the state variable."""
-
-    def incrementKF(i, params,post_exp,post_var, new_exog, new_obs):
+    def incrementKF(self, i, params,post_exp,post_var, new_exog, new_obs):
         """ unpack parameters """
-        unpacked = unpackParams(params)
+        unpacked = self.unpackParams(params)
         A = unpacked[0]
         V = unpacked[1]
         C = unpacked[2]
         W = unpacked[3]
-        B = unpacked[4][i,:]
-        sigma2MLE = unpacked[5]
-        postexpHold = postexpHold()
+        
+        #postexpHold = self.post_exp
 
         """ predict step"""
         prior_exp = A@post_exp
         prior_var = A@post_var@np.transpose(A) + V
         obs_prior_exp = C@prior_exp
         obs_prior_var = C@prior_var@np.transpose(C) + W
-        sum_exog = new_exog@B
+        if self.exog is None:
+            sum_exog = 0
+        else:
+            B = unpacked[4][i,:]
+            sigma2MLE = unpacked[5]
+            sum_exog = new_exog@B
 
         """ update step """
         residual = new_obs - obs_prior_exp - sum_exog
@@ -101,28 +113,34 @@ class PanelKalmanFilter():
         log_like = dist.logpdf(new_obs) + exodist.logpdf(residual)
         return [post_exp,post_var,log_like]#, post_exog]
 
-    """This function estimates a Kalman filter for each unit - sectors, regions, countries, groups, etc."""
+    """
+    This function estimates a Kalman filter for each unit - sectors, regions, countries, groups, etc.
+    """
 
-    def indivKF(params,init_exp,init_var,i):
+    def indivKF(self, params,init_exp,init_var,i):
         iData = endog.iloc[i,:]
-        iExog = []
-        for t in range(0,K):
-            iExog.append(exog[t].iloc[i,:])
-        iExog = pd.concat(iExog, axis = 1).transpose()
+        if self.exog is None:
+            iExog = None
+        else:
+            iExog = []
+            for t in range(0,self.K):
+                iExog.append(self.exog[t].iloc[i,:])
+            iExog = pd.concat(iExog, axis = 1).transpose()
+
         """ initialization """
         post_exp = init_exp
         post_var = init_var
 
-        init_obs = np.array(iData.loc[obsDict[0]])
-        init_obs_exog = np.array(iExog[obsDict[0]])
+        init_obs = np.array(iData.loc[self.obsDict[0]])
+        init_obs_exog = np.array(iExog[self.obsDict[0]])
         dist = stats.multivariate_normal(mean = init_obs, cov = 1)
         log_like = dist.logpdf(init_obs)
 
-        for t in range(0,(T-1)):
+        for t in range(0,(self.T-1)):
             """ predict and update """
-            new_obs = np.transpose(np.array(iData[obsDict[t+1]]))
-            new_exog = np.transpose(np.array(iExog[obsDict[t+1]]))
-            new_post = incrementKF(i, params,post_exp,post_var, new_exog, new_obs)
+            new_obs = np.transpose(np.array(iData[self.obsDict[t+1]]))
+            new_exog = np.transpose(np.array(iExog[self.obsDict[t+1]]))
+            new_post = self.incrementKF(i, params,post_exp,post_var, new_exog, new_obs)
             """ replace """
             post_exp = new_post[0]
             post_var = new_post[1]
@@ -131,38 +149,46 @@ class PanelKalmanFilter():
         log_like += new_post[2]
         return [np.sum(log_like), post_exp]
 
-    """This function goes through each unit to sum their log-likelihood contributions over time."""
+    """
+    This function goes through each unit to sum their log-likelihood contributions over time.
+    """
 
-    def sampleKF(params,init_exp,init_var):
+    def sampleKF(self, params,init_exp,init_var):
         post_exp = []
         log_like = 0.0
-        for i in range(0,N):
-            indivKFresults = indivKF(params,init_exp,init_var,i)
+        for i in range(0,self.N):
+            indivKFresults = self.indivKF(params,init_exp,init_var,i)
             log_like += indivKFresults[0]
             post_exp.append(indivKFresults[1])
-        neg_avg_log_like = -log_like/N
+        neg_avg_log_like = -log_like/self.N
         print("Current average negative log-likelihood: ",neg_avg_log_like)
         return [neg_avg_log_like, post_exp]
 
-    """This is a wrapper function for use with our optimization algorithm."""
+    """
+    This is a wrapper function for use with our optimization algorithm.
+    """
 
-    def wrapLoglike(params):
+    def wrapLoglike(self, params):
         #print("current parameters:{}".format(params))
-        sampler = sampleKF(params,init_exp,init_var)
+        sampler = self.sampleKF(params,self.init_exp,self.init_var)
         self.post_exp = post_exp = sampler[1]
         return sampler[0]
 
-    """We use the minimize function from the scipy package to perform Maximum Likelihood Estimation. This wrapper function allows convenience. It returns our parameter estimates and our state variable, for use in forecasting."""
+    """
+    We use the minimize function from the scipy package to perform Maximum Likelihood Estimation. 
+    This wrapper function allows convenience. 
+    It returns our parameter estimates and our state variable, for use in forecasting.
+    """
 
-    def wrapMinimize(params_init, algo):
+    def wrapMinimize(self, params_init, algo):
         MLE = minimize(fun = wrapLoglike, x0 = params_init, method = algo)
         return(MLE.x, self.post_exp)
 
     """This function forecasts the endogenous variable for time increments."""
 
-    def incrementForecast(i, params, new_exog, t, statevar):
+    def incrementForecast(self, i, params, new_exog, t, statevar):
         """ unpack parameters """
-        unpacked = unpackParams(params)
+        unpacked = self.unpackParams(params)
         A = unpacked[0]
         V = unpacked[1]
         C = unpacked[2]
@@ -180,18 +206,22 @@ class PanelKalmanFilter():
 
     """This function forecasts the endogenous variable per individual."""
 
-    def indivForecast(params,i, statevar):
-        iExog = []
-        for t in range(0,K):
-            iExog.append(exog_test[t].iloc[i,:])
-        iExog = pd.concat(iExog, axis = 1).transpose()
+    def indivForecast(self, params, i, statevar):
+        if self.exog is None:
+            iExog = None
+        else:
+            iExog = []
+            for t in range(0, self.K):
+                iExog.append(self.exog[t].iloc[i, :])
+            iExog = pd.concat(iExog, axis=1).transpose()
+
         obsDict = []
-        for t in range(0,int(len(iExog.columns)/(stateDim*obsDim))):
-            obsDict.append(list(iExog.columns[0+t*stateDim*obsDim:stateDim*obsDim*(t+1)]))
+        for t in range(0,int(len(iExog.columns)/(self.stateDim*self.obsDim))):
+            obsDict.append(list(iExog.columns[0+t*self.stateDim*self.obsDim:self.stateDim*self.obsDim*(t+1)]))
 
         new_obs = []
 
-        for t in range(0,len(obsDict)):
+        for t in range(0,len(self.obsDict)):
             """ predict and update """
             #reverse = list(reversed(range(len(obsDict))))
             new_exog = np.transpose(np.array(iExog[obsDict[t]]))
@@ -202,17 +232,28 @@ class PanelKalmanFilter():
 
     """This function is a wrapper function for forecasting."""
 
-    def forecastKalman(params, statevar):
+    def forecastKalman(self, params, statevar):
         forecast = []
-        for i in range(0,N):
-            indiv_forecast = indivForecast(params,i, statevar)
+        for i in range(0,self.N):
+            indiv_forecast = self.indivForecast(params,i, statevar)
             forecast.append(indiv_forecast)
         return forecast
 
-    def wrapForecast(params, statevar):
-        forecast = forecastKalman(params, statevar)
+    def wrapForecast(self, params, statevar):
+        forecast = self.forecastKalman(params, statevar)
         forecast_predict = []
         for t in range(len(forecast)):
             forecast_predict.append(pd.DataFrame(np.concatenate(forecast[t])).transpose())
         forecast_dataframe = pd.concat(forecast_predict)
         return(forecast_dataframe)
+
+    def paramInit(self):
+        # Define the number of parameters depending on whether exogenous data is provided
+        if self.exog is None:
+            num_params = 1 + self.stateDim**2 + self.stateDim + self.stateDim*self.obsDim + self.obsDim*self.stateDim + self.stateDim*self.obsDim
+        else:
+            num_params = 1 + self.stateDim**2 + self.stateDim + self.stateDim*self.obsDim + self.obsDim*self.stateDim + self.stateDim*self.obsDim + self.N*self.K
+
+        # Define an initial guess for the parameters
+        initial_guess = np.random.rand(num_params)
+        return initial_guess
